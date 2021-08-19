6bdeeed00eb89e48367704e0cb5496cae78fc3b8
Mohammad Jamiu <balogunmohammedjamiu@gmail.com>
2021-08-19 13:05:56 -0700

    init to github

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const harmburger = document.querySelector(".menu-icon");
+const lineOne = document.querySelector(".menu-line-one");
+const lineTwo = document.querySelector(".menu-line-two");
+const lineThree = document.querySelector(".menu-line-three");
+
+const nav = document.querySelector("header nav");
+
+harmburger.addEventListener("click", () => {
+  console.log("hello");
+
+  lineOne.classList.toggle("line-one");
+
+  lineThree.classList.toggle("line-three");
+  lineTwo.classList.toggle("line-two");
+  nav.classList.toggle("nav-active");
+});
+
+const tocIcon = document.querySelector(".toc-icon");
+const lcaToc = document.querySelector(".lca-toc");
+tocIcon.addEventListener("click", () => {
+  console.log("yes");
+  lcaToc.classList.toggle('toc-nav-active');
+});
